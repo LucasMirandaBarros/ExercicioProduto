612a7488fca8fb45943eff7c0d3eec2a6870de3d
LucasMiranda <lucaswespras@gmail.com>
2023-04-06 21:00:16 -0300

    Criação da pasta

---
+++ src/main/java/com/primeirotrmpodev/exercicioProduto/ExercicioProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrmpodev.exercicioProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioProdutoApplication.class, args);
+	}
+
+}
